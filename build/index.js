08b7416b1c08480e9a0b7d98b07264a4479c3ecc
Kevin Wade <kevin@kevinwade.name>
2015-12-27 09:56:01 -0600

    Add more icons

--- build/index.js
+++ build/index.js
@@ -109,7 +109,11 @@ return /******/ (function(modules) { // webpackBootstrap
 	// More icons: http://dmfrancisco.github.io/react-icons/
 	var paths = {
 	  'arrow-back': 'M20 11h-12.17l5.59-5.59-1.42-1.41-8 8 8 8 1.41-1.41-5.58-5.59h12.17v-2z',
-	  comment: 'M21.99 4c0-1.1-.89-2-1.99-2h-16c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14l4 4-.01-18zm-3.99 10h-12v-2h12v2zm0-3h-12v-2h12v2zm0-3h-12v-2h12v2z'
+	  'arrow-drop-down': 'M7 10l5 5 5-5z',
+	  'arrow-drop-up': 'M7 14l5-5 5 5z',
+	  clear: 'M19 6.41l-1.41-1.41-5.59 5.59-5.59-5.59-1.41 1.41 5.59 5.59-5.59 5.59 1.41 1.41 5.59-5.59 5.59 5.59 1.41-1.41-5.59-5.59z',
+	  comment: 'M21.99 4c0-1.1-.89-2-1.99-2h-16c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14l4 4-.01-18zm-3.99 10h-12v-2h12v2zm0-3h-12v-2h12v2zm0-3h-12v-2h12v2z',
+	  'file-upload': 'M9 16h6v-6h4l-7-7-7 7h4zm-4 2h14v2h-14z'
 	};
 
 	var Icon = (function (_React$Component) {
